1de1989ae0be6f8db27e033766efad2470e61bd4
Z <coolty3001@yahoo.com>
2020-09-20 22:18:05 -0400

    Level 3, 4, 5, and 6 cards.

--- NWN.FinalFantasy/Service/TripleTriad.cs
+++ NWN.FinalFantasy/Service/TripleTriad.cs
@@ -97,6 +97,61 @@ namespace NWN.FinalFantasy.Service
                 {CardType.Grendel, new Card("Grendel", "Card_P_Grendel", 4, 5, 2, 4)},
                 {CardType.Jelleye, new Card("Jelleye", "Card_P_Jelleye", 3, 1, 7, 2)},
                 {CardType.GrandMantis, new Card("Grand Mantis", "Card_P_GrandMant", 5, 5, 3, 2)},
+
+                // Level 3 Cards
+                {CardType.Forbidden, new Card("Forbidden", "Card_P_Forbidden", 6, 3, 2, 6)},
+                {CardType.Armadodo, new Card("Armadodo", "Card_P_Armadodo", 6, 1, 6, 3)},
+                {CardType.TriFace, new Card("Tri-Face", "Card_P_Tri-Face", 3, 5, 5, 5)},
+                {CardType.Fastitocalon, new Card("Fastitocalon", "Card_P_Fast1", 7, 1, 3, 5)},
+                {CardType.SnowLion, new Card("Snow Lion", "Card_P_SnowLion", 7, 5, 3, 1)},
+                {CardType.Ochu, new Card("Ochu", "Card_P_Ochu", 5, 3, 3, 6)},
+                {CardType.Sam08G, new Card("SAM08G", "Card_P_SAM08G", 5, 2, 4, 6)},
+                {CardType.DeathClaw, new Card("Death Claw", "Card_P_DeathClaw", 4, 7, 2, 4)},
+                {CardType.Cactuar, new Card("Cactuar", "Card_P_Cactuar", 6, 3, 2, 6)},
+                {CardType.Tonberry, new Card("Tonberry", "Card_P_Tonberry", 3, 4, 4, 6)},
+                {CardType.AbyssWorm, new Card("Abyss Worm", "Card_P_AbyssWorm", 7, 3, 5, 2)},
+
+                // Level 4 Cards
+                {CardType.Turtapod, new Card("Turtapod", "Card_P_Turtapod", 2, 6, 7, 3)},
+                {CardType.Vysage, new Card("Vysage", "Card_P_Vysage", 6, 4, 5, 5)},
+                {CardType.TRexaur, new Card("T-Rexaur", "Card_P_TRexaur", 4, 2, 7, 6)},
+                {CardType.Bomb, new Card("Bomb", "Card_P_Bomb", 2, 6, 3, 7)},
+                {CardType.Blitz, new Card("Blitz", "Card_P_Blitz", 1, 4, 7, 6)},
+                {CardType.Wendigo, new Card("Wendigo", "Card_P_Wendigo", 7, 1, 6, 3)},
+                {CardType.Torama, new Card("Torama", "Card_P_Torama", 7, 4, 4, 4)},
+                {CardType.Imp, new Card("Imp", "Card_P_Imp", 6, 3, 2, 6)},
+                {CardType.BlueDragon, new Card("Blue Dragon", "Card_P_BlueDrago", 6, 7, 3, 2)},
+                {CardType.Adamantoise, new Card("Adamantoise", "Card_P_Adamantoi", 4, 5, 6, 5)},
+                {CardType.Hexadragon, new Card("Hexadragon", "Card_P_Hexadrago", 7, 4, 3, 5)},
+
+                // Level 5 Cards
+                {CardType.IronGiant, new Card("Iron Giant", "Card_P_IronGiant", 6, 6, 5, 5)},
+                {CardType.Behemoth, new Card("Behemoth", "Card_P_Behemoth", 3, 5, 7, 6)},
+                {CardType.Chimera, new Card("Chimera", "Card_P_Chimera", 6, 3, 2, 6)},
+                {CardType.PuPu, new Card("PuPu", "Card_P_PuPu", 3, 2, 1, 10)},
+                {CardType.Elastoid, new Card("Elastoid", "Card_P_Elastoid", 6, 6, 7, 2)},
+                {CardType.GIM47N, new Card("GIM47N", "Card_P_GIM47N", 5, 7, 4, 5)},
+                {CardType.Malboro, new Card("Malboro", "Card_P_Malboro", 7, 4, 2, 7, CardElementType.Poison)},
+                {CardType.RubyDragon, new Card("Ruby Dragon", "Card_P_RubyDrago", 7, 7, 4, 2)},
+                {CardType.Elnoyle, new Card("Elnoyle", "Card_P_Elnoyle", 5, 7, 6, 3)},
+                {CardType.TonberryKing, new Card("Tonberry King", "Card_P_TonberryK", 4, 7, 4, 6)},
+                {CardType.BiggsWedge, new Card("Biggs, Wedge", "Card_P_BiggsWedg", 6, 2, 7, 6)},
+
+                // Level 6 Cards
+                {CardType.FujinRaijin, new Card("Fujin, Raijin", "Card_P_FujinRaij", 2, 8, 4, 8)},
+                {CardType.Elvoret, new Card("Elvoret", "Card_P_Elvoret", 7, 3, 4, 8)},
+                {CardType.XATM092, new Card("X-ATM092", "Card_P_XATM092", 4, 7, 3, 8)},
+                {CardType.Granaldo, new Card("Granaldo", "Card_P_Granaldo", 7, 8, 5, 2)},
+                {CardType.Gerogero, new Card("Gerogero", "Card_P_Gerogero", 1, 8, 3, 8, CardElementType.Poison)},
+                {CardType.Iguion, new Card("Iguion", "Card_P_Iguion", 8, 8, 2, 2)},
+                {CardType.Abadon, new Card("Abadon", "Card_P_Abadon", 6, 4, 5, 8)},
+                {CardType.Trauma, new Card("Trauma", "Card_P_Trauma", 4, 5, 6, 8)},
+                {CardType.Oilboyle, new Card("Oilboyle", "Card_P_Oilboyle", 1, 4, 8, 8)},
+                {CardType.ShumiTribe, new Card("Shumi Tribe", "Card_P_ShumiTrib", 6, 8, 4, 5)},
+                {CardType.Krysta, new Card("Krysta", "Card_P_Krysta", 7, 8, 1, 5)},
+
+                // Level 7 Cards
+
             };
         }
 
